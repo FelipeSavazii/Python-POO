07083f75956555ded8a8bcbcec82eae3470d9646
FelipeSavazi <90295858+FelipeSavazii@users.noreply.github.com>
2021-10-18 18:43:44 -0300

    Create exercicio-04.py

---
+++ exercicio-04.py
@@ -0,0 +1,17 @@
+class Rectangle:
+    def __init__(self, length, width):
+        self.__length = length
+        self.__width = width
+
+    def area(self):
+        return(self.__length * self.__width)
+
+    def perimeter(self):
+        return(2 * (self.__length + self.__width))
+
+length = int(input('Digite o comprimento: '))
+width = int(input('Digite a largura: '))
+
+retangulo = Rectangle(length, width)
+print("A área é ", retangulo.area())
+print("O perimetro é ", retangulo.perimeter())
